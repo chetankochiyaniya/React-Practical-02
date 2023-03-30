884235aed89e10c520c724aab6956509a8baf8bd
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-30 15:54:34 +0530

    Updated checkbox checked icon

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -4,47 +4,47 @@ import './Task.css'
 // Functional component with hooks
 export default function Task() {
 
-    const [showInput,setShowInput] =  useState(false);
+    const [showInput, setShowInput] = useState(false);
 
     useEffect(() => {
         window.addEventListener('keydown', handleKeyPress);
-    
-        return () => {
-          window.removeEventListener('keydown', handleKeyPress);
-        };
-      }, [showInput]);
 
-    const addTask = ()=>{
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [showInput]);
+
+    const addTask = () => {
         setShowInput(!showInput);
     }
 
     const handleKeyPress = (event) => {
-        console.log("enter handlekey funcation",event.key)
+        console.log("enter handlekey funcation", event.key)
         if (event.key === 'Escape') {
-            setShowInput(!showInput);
+            setShowInput(false);
         }
-      };
+    };
     return <>
-    <div className="task-list-container">
-        <div className='task'>
-            <div className='task-msg'>Buy new sweatshirt </div><input type='checkbox' className='task-checkbox' defaultChecked={false} />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Begin promotional phase</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Read an article</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Try not fall asleep</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Begin QA for the product</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Go for a walk</div><input type='checkbox' />
-        </div>
-        <div className='task'>
+        <div className="task-list-container">
+            <div className='task'>
+                <div className='task-msg'>Buy new sweatshirt </div><input type='checkbox' className='task-checkbox' defaultChecked={false} />
+            </div>
+            <div className='task'>
+                <div className='task-msg'>Begin promotional phase</div><input type='checkbox' />
+            </div>
+            <div className='task'>
+                <div className='task-msg'>Read an article</div><input type='checkbox' />
+            </div>
+            <div className='task'>
+                <div className='task-msg'>Try not fall asleep</div><input type='checkbox' />
+            </div>
+            <div className='task'>
+                <div className='task-msg'>Begin QA for the product</div><input type='checkbox' />
+            </div>
+            <div className='task'>
+                <div className='task-msg'>Go for a walk</div><input type='checkbox' />
+            </div>
+            {/* <div className='task'>
             <div className='task-msg'>Buy new sweatshirt</div><input type='checkbox' className='task-checkbox' defaultChecked={false} />
         </div>
         <div className='task'>
@@ -97,15 +97,15 @@ export default function Task() {
         </div>
         <div className='task'>
             <div className='task-msg'>Go for a walk</div><input type='checkbox' />
+        </div> */}
         </div>
-    </div>
-    { showInput ? (<div>
-        <input type='text' className='input-size'/>
-    </div>):(
-        <div className='add-todo-btn-container' >
-            <button className='add-todo-button' onClick={()=>addTask()}>
-                +
-            </button>
-        </div>)}
+        {showInput ? (<div>
+            <input type='text' className='input-size' placeholder=' Press "Esc" to exit' />
+        </div>) : (
+            <div className='add-todo-btn-container' >
+                <button className='add-todo-button' onClick={() => addTask()}>
+                    +
+                </button>
+            </div>)}
     </>
 }
