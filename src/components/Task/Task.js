e2ed4a169493668a09d3d27fb27dbdb86f0c3fbd
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-03-30 17:44:16 +0530

    added checked task functionality

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,111 +1,156 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
+// import Button from '../Button/Button'
+// import './Task.css'
+
+// // Functional component with hooks
+// export default function Task() {
+//     const [list, setList] = useState(["Buy a new product",
+//         "Begin promotional phase",
+//         "Read an article",
+//         "Try not to fall asleep",
+//         "Begin QA with product",
+//         "Go for a walk"])
+
+
+//     return <>
+//         <div className="task-list-container">
+//             {list.map((text, ind) => (
+//                 <>
+//                     <div className='task' id={`task-${ind}`}>
+//                         <div className='task-msg' id={`text-${ind}`}>{text}</div>
+//                         <input type='checkbox' id={`checkbox-${ind}`} className='task-checkbox' />
+//                     </div>
+//                 </>))}
+//         </div>
+//         <Button />
+//     </>
+// }
+
+
+// import { useState } from 'react'
+// import './Task.css'
+
+// export default function Task() {;
+//     const [list, setList] = useState([
+//         "Buy a new product",
+//         "Begin promotional phase",
+//         "Read an article",
+//         "Try not to fall asleep",
+//         "Begin QA with product",
+//         "Go for a walk"
+//     ]);
+
+//     const handleCheckboxChange = (index, checked) => {
+//         const newList = [...list];
+//         newList[index] = {
+//             text: newList[index],
+//             checked: checked
+//         };
+//         setList(newList);
+//     };
+
+//     return (
+//         <>
+//             <div className="task-list-container">
+//                 {list.map((item, index) => (
+//                     <div className='task' key={index}>
+//                         <div className={`task-msg ${item.checked ? 'checked' : ''}`}>
+//                             {item.text}
+//                         </div>
+//                         <input
+//                             type='checkbox'
+//                             className='task-checkbox'
+//                             onChange={(event) => handleCheckboxChange(index, event.target.checked)}
+//                             checked={item.checked}
+//                         />
+//                     </div>
+//                 ))}
+//             </div>
+//         </>
+//     );
+// }
+
+
+// import { useState } from 'react'
+// import './Task.css'
+
+// export default function Task() {
+//     const [list, setList] = useState([
+//         { text: "Buy a new product", checked: false },
+//         { text: "Begin promotional phase", checked: false },
+//         { text: "Read an article", checked: false },
+//         { text: "Try not to fall asleep", checked: false },
+//         { text: "Begin QA with product", checked: false },
+//         { text: "Go for a walk", checked: false }
+//     ]);
+
+//     const handleCheckboxChange = (index, checked) => {
+//         const newList = [...list];
+//         newList[index] = { ...newList[index], checked: checked };
+//         setList(newList);
+//     };
+
+//     return (
+//         <>
+//             <div className="task-list-container">
+//                 {list.map((item, index) => (
+//                     <div className='task' key={index}>
+//                         <div className={`task-msg ${item.checked ? 'checked' : ''}`}>
+//                             {item.text}
+//                         </div>
+//                         <input
+//                             type='checkbox'
+//                             className='task-checkbox'
+//                             onChange={(event) => handleCheckboxChange(index, event.target.checked)}
+//                             checked={item.checked}
+//                         />
+//                     </div>
+//                 ))}
+//             </div>
+//         </>
+//     );
+// }
+
+import { useState } from 'react'
+import Button from '../Button/Button'
 import './Task.css'
 
-// Functional component with hooks
-export default function Task() {
+export default function Task() {;
+    const [list, setList] = useState([
+        { text: "Buy a new product", checked: false },
+        { text: "Begin promotional phase", checked: false },
+        { text: "Read an article", checked: false },
+        { text: "Try not to fall asleep", checked: false },
+        { text: "Begin QA with product", checked: false },
+        { text: "Go for a walk", checked: false }
+    ]);
 
-    const [showInput, setShowInput] = useState(false);
-
-    useEffect(() => {
-        window.addEventListener('keydown', handleKeyPress);
-
-        return () => {
-            window.removeEventListener('keydown', handleKeyPress);
+    const handleCheckboxChange = (index, checked) => {
+        const newList = [...list];
+        newList[index] = {
+            text: newList[index].text,
+            checked: checked
         };
-    }, [showInput]);
-
-    const addTask = () => {
-        setShowInput(!showInput);
-    }
-
-    const handleKeyPress = (event) => {
-        console.log("enter handlekey funcation", event.key)
-        if (event.key === 'Escape') {
-            setShowInput(false);
-        }
+        setList(newList);
     };
-    return <>
-        <div className="task-list-container">
-            <div className='task'>
-                <div className='task-msg'>Buy new sweatshirt </div><input type='checkbox' className='task-checkbox' defaultChecked={false} />
+
+    return (
+        <>
+            <div className="task-list-container">
+                {list.map((item, index) => (
+                    <div className='task' key={index}>
+                        <div className={`task-msg ${item.checked ? 'checked' : ''}`}>
+                            {item.text}
+                        </div>
+                        <input
+                            type='checkbox'
+                            onChange={(event) => handleCheckboxChange(index, event.target.checked)}
+                            checked={item.checked}
+                        />
+                    </div>
+                ))}
             </div>
-            <div className='task'>
-                <div className='task-msg'>Begin promotional phase</div><input type='checkbox' />
-            </div>
-            <div className='task'>
-                <div className='task-msg'>Read an article</div><input type='checkbox' />
-            </div>
-            <div className='task'>
-                <div className='task-msg'>Try not fall asleep</div><input type='checkbox' />
-            </div>
-            <div className='task'>
-                <div className='task-msg'>Begin QA for the product</div><input type='checkbox' />
-            </div>
-            <div className='task'>
-                <div className='task-msg'>Go for a walk</div><input type='checkbox' />
-            </div>
-            {/* <div className='task'>
-            <div className='task-msg'>Buy new sweatshirt</div><input type='checkbox' className='task-checkbox' defaultChecked={false} />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Begin promotional phase</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Read an article</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Try not fall asleep</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Begin QA for the product</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Go for a walk</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Buy new sweatshirt </div><input type='checkbox' className='task-checkbox' defaultChecked={false} />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Begin promotional phase</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Read an article</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Try not fall asleep</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Begin QA for the product</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Go for a walk</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Buy new sweatshirt</div><input type='checkbox' className='task-checkbox' defaultChecked={false} />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Begin promotional phase</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Read an article</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Try not fall asleep</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Begin QA for the product</div><input type='checkbox' />
-        </div>
-        <div className='task'>
-            <div className='task-msg'>Go for a walk</div><input type='checkbox' />
-        </div> */}
-        </div>
-        {showInput ? (<div>
-            <input type='text' className='input-size' placeholder=' Press "Esc" to exit' />
-        </div>) : (
-            <div className='add-todo-btn-container' >
-                <button className='add-todo-button' onClick={() => addTask()}>
-                    +
-                </button>
-            </div>)}
-    </>
+            <Button />
+        </>
+    );
 }
